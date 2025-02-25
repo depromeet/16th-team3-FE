5c9ed6249aacd5fccab1d1c97c58099c58db93d1
Yeom <76275691+prgmr99@users.noreply.github.com>
2025-02-25 18:06:04 +0900

    SCRUM-85 feat: 마감 시간 선택 기능 구현 (#18)
    * SCRUM-85 feat: 마감시간 기능 작업 / 일부 기능 개선
    
    * SCRUM-85 fix: Drawer 속 버튼 에러 수정 / Drawer 드래그 disable
    
    * SCRUM-85 feat: 버튼 비활성화 로직 적용

--- src/components/clearableInput/ClearableInput.tsx
+++ src/components/clearableInput/ClearableInput.tsx
@@ -1,37 +1,60 @@
 import { cn } from '@/lib/utils';
 import { Input } from '../ui/input';
-import { RefObject } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Image from 'next/image';
 
 interface ClearableInputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   value: string;
-  ref: RefObject<HTMLInputElement | null>;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
+
+const WAITING_TIME = 200;
 
 const ClearableInput = ({
   className,
   value,
-  ref,
   onChange,
   ...props
 }: ClearableInputProps) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const [isFocused, setIsFocused] = useState(true);
+
+  useEffect(() => {
+    if (inputRef.current)
+      setTimeout(() => {
+        if (inputRef.current) {
+          inputRef.current.focus();
+          setIsFocused(true);
+        }
+      }, WAITING_TIME);
+  }, []);
+
   return (
     <div className="relative w-full">
-      <span className="text-primary b3">할 일 입력</span>
+      <span
+        className={cn(
+          'b3 transition-colors',
+          isFocused ? 'text-primary' : 'text-neutral',
+        )}
+      >
+        할 일 입력
+      </span>
+
       <Input
         className={cn(
           'focus-within: t3 w-full border-0 border-b bg-transparent pl-0 pr-10 transition-colors focus:border-b-2 focus:border-b-component-accent-primary',
           className,
         )}
         value={value}
-        ref={ref}
+        ref={inputRef}
+        onFocus={() => setIsFocused(true)}
+        onBlur={() => setIsFocused(false)}
         onChange={onChange}
         {...props}
       />
 
-      {value && (
+      {value && isFocused && (
         <button
           type="button"
           className="absolute right-0 top-[24px] translate-y-1/4 text-black hover:text-gray-600"
