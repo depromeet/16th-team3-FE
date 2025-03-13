b2625a1ad3f87b7e882d18775a382fdafb4b8b44
prgmr99 <johnyeom24@naver.com>
2025-03-13 21:21:07 +0900

    SCRUM-153 test: 테스트 콘솔 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -106,7 +106,7 @@ export async function middleware(request: NextRequest) {
 export const config = {
   // * Next.js 내부 리소스를 제외한 모든 경로에 middleware 적용
   // * 로그인 페이지는 제외 - 제외하지 않으면 무한 리다이렉트 발생
-  matcher: ['/', '/((?!_next/static|favicon.ico|login|icons).*)'],
+  matcher: ['/'],
 };
 
 export const runtime = 'nodejs';
