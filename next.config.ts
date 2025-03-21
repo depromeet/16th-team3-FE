a1fa1fa8aa5bc59470986053c3d9a84308833984
prgmr99 <johnyeom24@naver.com>
2025-03-21 17:23:04 +0900

    fix: profile image rendering error

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'k.kakaocdn.net',
+        hostname: 'img1.kakaocdn.net',
         pathname: '/**',
       },
     ],
