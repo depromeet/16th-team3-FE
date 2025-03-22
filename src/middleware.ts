1001c551ccc184c2c4896fb52ce431b227b6c307
prgmr99 <johnyeom24@naver.com>
2025-03-23 08:45:02 +0900

    chore: console.log 제거

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,7 +24,6 @@ export async function middleware(request: NextRequest) {
 	}
 
 	if (!isOpenPath && !refreshToken) {
-		console.log("hi");
 		return NextResponse.redirect(new URL("/login", request.url));
 	}
 
