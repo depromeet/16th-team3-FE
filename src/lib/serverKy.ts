b2a3e640c80b12cf56efff165f319ff6916d75af
prgmr99 <johnyeom24@naver.com>
2025-03-25 00:57:52 +0900

    fix: 307 에러를 해결하기 위한 파일 구조 변경 수정

--- src/lib/serverKy.ts
+++ src/lib/serverKy.ts
@@ -12,6 +12,11 @@ export const serverApi = ky.create({
 	headers: {
 		"Content-Type": "application/json",
 	},
+	retry: {
+		limit: 2,
+		methods: ["get", "post"],
+		statusCodes: [401, 408, 413, 429, 500, 502, 503, 504],
+	},
 	hooks: {
 		beforeRequest: [
 			async (request) => {
