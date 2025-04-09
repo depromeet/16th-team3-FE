461e1029c948ba5f783e4d90cd70eac08f47a2ac
prgmr99 <johnyeom24@naver.com>
2025-04-09 23:44:27 +0900

    SCRUM-182 chore: 완료한 일의 시간 추가

--- src/app/(protected)/my-page/_component/TaskContainer.tsx
+++ src/app/(protected)/my-page/_component/TaskContainer.tsx
@@ -1,13 +1,22 @@
 import type { TaskOrigin } from "@/types/myPage";
+import { format } from "date-fns";
+import { ko } from "date-fns/locale";
 import React, { useState } from "react";
 
-const TaskItem = ({ task }: { task: TaskOrigin }) => (
-	// TODO : 여기에 완료한 일 또는 미룬 일 눌렀을때 해당 화면 전환 함수 추가 필요
-	<div className="flex flex-col gap-2 py-4">
-		<div className="text-s2">{task.name}</div>
-		<div className="text-s3 text-gray-alternative">{task.due}</div>
-	</div>
-);
+const TaskItem = ({ task }: { task: TaskOrigin }) => {
+	const date = new Date(task.dueDatetime);
+	const formattedDate = format(date, "M월 d일 (eee)ㆍa hh:mm까지", {
+		locale: ko,
+	});
+
+	return (
+		// TODO : 여기에 완료한 일 또는 미룬 일 눌렀을때 해당 화면 전환 함수 추가 필요
+		<div className="flex flex-col gap-2 py-4">
+			<div className="text-s2">{task.name}</div>
+			<div className="text-s3 text-gray-alternative">{formattedDate}</div>
+		</div>
+	);
+};
 
 const TaskList = ({ tasks }: { tasks: TaskOrigin[] }) => (
 	<>
