ea15a4576cbb3087dd4aaa96e69fa168bb9e6587
Juhyun Kim <juhyun_kim6@tmax.co.kr>
2025-04-16 18:11:20 +0900

    feat: 마이페이지 작업 상세 - 문구 및 캐릭터

--- src/app/(protected)/my-page/task-detail/[taskId]/ExpiredTaskDetailPage.tsx
+++ src/app/(protected)/my-page/task-detail/[taskId]/ExpiredTaskDetailPage.tsx
@@ -13,33 +13,57 @@ type Props = {
 
 export default function ExpiredTaskDetailPage({task} :Props) {
     const router = useRouter();
-    const currentTask = useExpiredTaskStore((state) => state.currentTask);
-    // if (!currentTask) {
-    //     router.push("/my-page");
-    // }
-    // const task = currentTask as TaskOrigin;
-    // console.log(task)
+    console.log(task);
+
+    const taskStatus = task.status === "COMPLETE" ? "COMPLETE" : "FAIL";
+
+    const PHRASE = {
+        COMPLETE : {
+            topBar : "완료한 일",
+            main: "잘 완료하셨어요!"
+        },
+        FAIL : {
+            topBar : "미룬 일",
+            main: "완료를 안하셨네요. 다음엔 꼭 완료해요!"
+        }
+    }
 
     return (
         <div className="flex min-h-screen flex-col pb-[34px]">
             {/* 헤더 부분 */}
-            <CustomBackHeader title={task.status === "COMPLETE" ? "완료한 일" : "미룬 일"} backRoute="/my-page">
+            <CustomBackHeader title={PHRASE[taskStatus].topBar} backRoute="/my-page">
             </CustomBackHeader>
 
             {/* Contents 부분 */}
-            <div className="mb-8 mt-[65px] flex flex-col gap-5 items-center justify-center">
-                {task.name}
+            <div className="mb-8 mt-[54px] flex flex-col gap-5 mx-5 justify-center">
                 {/* Contents - 작업 개요 */}
                 <div className="flex flex-col">
                     {/* Contents - 작업 개요 - 문구*/}
-                    <div>
-                        문구
+                    <div className="t3 flex mt-4 mb-5 justify-start">
+                        <p>
+                            {task.name} <br />
+                            {PHRASE[taskStatus].main}
+                        </p>
                     </div>
 
-
                     {/* Contents - 작업 개요 - 작업 정보 */}
-                    <div>
-                        작업 정보
+                    <div className="flex flex-col gap-6">
+                        {/* Contents - 작업 개요 - 작업 정보 - 페르소나 */}
+                        <div className="flex flex-col gap-3">
+                            <div className="relative h-[120px]">
+                                <Image
+                                    src="/icons/mypage/mypage-character.png"
+                                    alt="mypage-character"
+                                    fill
+                                />
+                            </div>
+                        </div>
+
+                        {/* Contents - 작업 개요 - 작업 정보 - 페르소나 제외 작업 정보 */}
+                        <div>
+
+                        
+                        </div>
                     </div>
                 </div>
             </div>
