29b7583b2d1f75d3727b3fe7f3a84dafeea55bae
prgmr99 <johnyeom24@naver.com>
2025-05-03 22:07:29 +0900

    fix: 회고 만족도 잘못된 데이터 보내는 이슈 수정

--- src/app/(protected)/retrospection/[taskId]/RetrospectionPageClient.tsx
+++ src/app/(protected)/retrospection/[taskId]/RetrospectionPageClient.tsx
@@ -164,7 +164,7 @@ export default function RetrospectionPageClient({ task }: Props) {
 							taskId: task.id,
 							retrospection: {
 								...retrospectContent,
-								satisfaction: retrospectContent.satisfaction * 20,
+								satisfaction: (retrospectContent.satisfaction + 1) * 20,
 								concentration: retrospectContent.concentration * 20,
 							},
 						})
